cad598708bff8a6bed8e2bab2855db0d09362190
Alix Shuk Han Chau <86642948+Alixchau@users.noreply.github.com>
2021-11-10 16:00:53 -0700

    add adminFilter & map filter chain

---
+++ src/java/filters/AdminFilter.java
@@ -0,0 +1,33 @@
+package filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ *
+ * @author Alix
+ */
+public class AdminFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
